58c6c20a167896f2eb6b8708b149d8343caec078
elabs <elijahknsubuga@gmail.com>
2022-04-15 18:39:32 +0300

    Added a dashboard with equipment

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:medtrain/splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'onboard/onboard.dart';
 
 int? isViewed;
--- lib/page/dashboard.dart
+++ lib/page/dashboard.dart
@@ -1,12 +1,147 @@
+import 'dart:math' as math;
+import 'package:url_launcher/link.dart';
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
+  const DashboardPage({Key? key}) : super(key: key);
+
+  @override
+  State createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<DashboardPage> {
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Dashboard Page"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Explore!!!"),
+      ),
+      body: ExpandableTheme(
+        data: const ExpandableThemeData(
+          iconColor: Colors.blue,
+          useInkWell: true,
+        ),
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          children: <Widget>[
+            const Card1(),
+            /*Centrifuge(),
+            Defibrillators(),*/
+          ],
+        ),
       ),
     );
   }
 }
+
+class Card1 extends StatelessWidget {
+  const Card1({Key? key}) : super(key: key);
+  launchURL(String url) async{
+    if(await canLaunch(url)){
+      await launch(url);
+    }
+    else{
+      throw 'Could not open tutorial $url';
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return ExpandableNotifier(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 150,
+                  child: Container(
+                    child: Image.asset(
+                      'assets/images/330.png',
+                      height: 300.0,
+                      width: 300.0,
+                    ),
+                  ),
+                ),
+                ScrollOnExpand(
+                  scrollOnExpand: true,
+                  scrollOnCollapse: false,
+                  child: ExpandablePanel(
+                    theme: const ExpandableThemeData(
+                      headerAlignment: ExpandablePanelHeaderAlignment.center,
+                      tapBodyToCollapse: true,
+                    ),
+                    header: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "XN-330",
+                          )),
+                    collapsed: ElevatedButton(
+                      child: const Text('Tutorial'),
+                      onPressed: (){
+                        const url = 'https://www.youtube.com/watch?v=FxTwIYbflEI';
+                        launchURL(url);
+                        },
+                    ),
+                    expanded: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Clean with a clean cloth',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                            ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Incase of a break down, please call a Sysmex engineer',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                      ],
+                    ),
+                    builder: (_, collapsed, expanded) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                        child: Expandable(
+                          collapsed: collapsed,
+                          expanded: expanded,
+                          theme: const ExpandableThemeData(crossFadePoint: 0),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+/*
+
+class Centrifuge extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+class Defibrillators extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+*/
