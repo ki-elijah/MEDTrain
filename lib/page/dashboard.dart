c48d3e127fdeeb8eab99795da9dde731e839131d
elabs <elijahknsubuga@gmail.com>
2022-04-15 18:50:11 +0300

    Added a dashboard with equipment

--- lib/page/dashboard.dart
+++ lib/page/dashboard.dart
@@ -29,9 +29,9 @@ class MyHomePageState extends State<DashboardPage> {
         child: ListView(
           physics: const BouncingScrollPhysics(),
           children: <Widget>[
-            const Card1(),
-            /*Centrifuge(),
-            Defibrillators(),*/
+            const xn330(),
+            Centrifuge(),
+            /*Defibrillators(),*/
           ],
         ),
       ),
@@ -39,8 +39,8 @@ class MyHomePageState extends State<DashboardPage> {
   }
 }
 
-class Card1 extends StatelessWidget {
-  const Card1({Key? key}) : super(key: key);
+class xn330 extends StatelessWidget {
+  const xn330({Key? key}) : super(key: key);
   launchURL(String url) async{
     if(await canLaunch(url)){
       await launch(url);
@@ -127,15 +127,112 @@ class Card1 extends StatelessWidget {
         ));
   }
 }
-/*
 
 class Centrifuge extends StatelessWidget {
+  const Centrifuge({Key? key}) : super(key: key);
+  launchURL(String url) async{
+    if(await canLaunch(url)){
+      await launch(url);
+    }
+    else{
+      throw 'Could not open tutorial $url';
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return ExpandableNotifier(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 150,
+                  child: Container(
+                    child: Image.asset(
+                      'assets/images/centrifuges.jpg',
+                      height: 300.0,
+                      width: 300.0,
+                    ),
+                  ),
+                ),
+                ScrollOnExpand(
+                  scrollOnExpand: true,
+                  scrollOnCollapse: false,
+                  child: ExpandablePanel(
+                    theme: const ExpandableThemeData(
+                      headerAlignment: ExpandablePanelHeaderAlignment.center,
+                      tapBodyToCollapse: true,
+                    ),
+                    header: const Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Laboratory Centrifuges",
+                        )),
+                    collapsed: ElevatedButton(
+                      child: const Text('Tutorial'),
+                      onPressed: (){
+                        const url = 'https://www.youtube.com/watch?v=E0Kt79N9Sm4';
+                        launchURL(url);
+                      },
+                    ),
+                    expanded: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Remove all tube adapters from the buckets.',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Wash buckets and adaptors in warm soap water. Mild multi-purpose cleaner or dish-washing liquid are well suited.',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Use a sponge and a plastic brush. Metal brushes scratch the surface, which createproblems with corrosion in the future.',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10),
+                          child: Text(
+                            'Only use kitchen scouring pads in extreme cases. But watch out, do not scratch thesurfaces.',
+                            softWrap: true,
+                            overflow: TextOverflow.fade,
+                          ),
+                        ),
+                      ],
+                    ),
+                    builder: (_, collapsed, expanded) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                        child: Expandable(
+                          collapsed: collapsed,
+                          expanded: expanded,
+                          theme: const ExpandableThemeData(crossFadePoint: 0),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
+/*
 
 class Defibrillators extends StatelessWidget {
   @override
